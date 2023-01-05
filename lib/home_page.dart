0906484b0ced4a4470f12d9b77e751bfaae96797
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-01-05 08:53:15 +0100

    Changed appbar title and added action to change app theme

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,12 +11,18 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material App Bar'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text(
+            'Gee AI',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.wb_sunny))
+          ],
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(16),
+          child: SafeArea(child: SingleChildScrollView()),
+        ));
   }
 }
