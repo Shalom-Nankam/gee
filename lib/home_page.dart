96df344164fc4ddfda46f523e69d5376416bdbbd
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-01-05 08:58:26 +0100

    Added the FAB button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,18 +11,28 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Gee AI',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.wb_sunny))
-          ],
+      appBar: AppBar(
+        title: const Text(
+          'Gee AI',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(16),
-          child: SafeArea(child: SingleChildScrollView()),
-        ));
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.wb_sunny))
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: SafeArea(child: SingleChildScrollView()),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Icon(
+          Icons.note_outlined,
+          size: 20,
+        ),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+    );
   }
 }
