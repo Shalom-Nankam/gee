79c6028c1e096d7da9c5a5c37c6aefddf7f34530
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-01-07 21:50:23 +0100

    Added the speech to text plugin, developed home screen

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,11 +20,40 @@ class _HomeState extends State<Home> {
           IconButton(onPressed: () {}, icon: const Icon(Icons.wb_sunny))
         ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16),
-        child: SafeArea(child: SingleChildScrollView()),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SafeArea(
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 70,
+              ),
+              const Text('Tap the button to begin recording',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff000000))),
+              const SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                height: 100,
+                width: double.infinity,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.mic,
+                      size: 100,
+                    )),
+              )
+            ],
+          ),
+        )),
       ),
       floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: const Color(0xff000000),
         onPressed: () {},
         label: const Icon(
           Icons.edit_note,
