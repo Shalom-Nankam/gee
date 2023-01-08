36635224fb8d99fb01d7b9f4f04df5e8ecaa3862
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-01-08 16:34:29 +0100

    Set up and initialized speech to text plugins and methods

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gee/views/home_view.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +11,42 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeController extends State<HomeScreen> {
+  SpeechToText userSpeech = SpeechToText();
+  bool speechToTextIsEnabled = false;
+  bool isRecordingSPeech = false;
+  String convertedSPeechToText = '';
+
+  @override
+  void initState() {
+    initializeSPeechToText();
+    super.initState();
+  }
+
+  void initializeSPeechToText() async {
+    speechToTextIsEnabled = await userSpeech.initialize();
+    setState(() {});
+  }
+
+  void recordSpeech() async {
+    await userSpeech.listen(onResult: speechResult);
+    setState(() {
+      isRecordingSPeech = true;
+    });
+  }
+
+  void stopRecordingSpeech() async {
+    await userSpeech.stop();
+    setState(() {
+      isRecordingSPeech = false;
+    });
+  }
+
+  void speechResult(SpeechRecognitionResult result) {
+    setState(() {
+      convertedSPeechToText = result.recognizedWords;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return HomeView(this);
