569bc8ea188710f75f3ebba1183d83d5361f4645
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-01-11 15:38:03 +0100

    New reusable widget

---
+++ lib/widgets/search_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SearchButton extends StatelessWidget {
+  const SearchButton({super.key, required this.pressAction});
+  final VoidCallback pressAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: 200,
+      child: OutlinedButton(
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(const Color(0xff000000))),
+          onPressed: pressAction,
+          child: const Text(
+            'Go',
+            style: TextStyle(
+                color: Color(0xffFFFFFF),
+                fontWeight: FontWeight.bold,
+                fontSize: 20),
+          )),
+    );
+  }
+}
