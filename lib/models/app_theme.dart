f86863131eb141db5c6914b888373bad6c393c2d
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-01-16 14:14:56 +0100

    Renamed the theme variable to reflect the dark mode

--- lib/models/app_theme.dart
+++ lib/models/app_theme.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppTheme {
-  static bool themeMode = false;
+  static bool darkMode = false;
   static getTheme() async {
     final prefs = await SharedPreferences.getInstance();
-    themeMode = prefs.getBool('Theme') ?? false;
+    darkMode = prefs.getBool('Theme') ?? false;
   }
 
   static ThemeData setAppTheme() {
